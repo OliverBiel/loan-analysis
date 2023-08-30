281f30bab641d0c5c38977c1b16dc3eba9dcefff
oliverbiel <gmartins.oliver@gmail.com>
2023-08-30 13:22:15 -0300

    refactor(entrypoint): rodar testes

--- api/entrypoint.sh
+++ api/entrypoint.sh
@@ -3,4 +3,5 @@ python -m celery -A api worker -l info &
 python -m celery -A api beat -l info &
 python /app/manage.py migrate
 python /app/manage.py loaddata base.json
+python /app/manage.py test
 python /app/manage.py runserver 0.0.0.0:8000
